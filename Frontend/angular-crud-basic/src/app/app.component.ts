9f269d570fc90a4bbfc21db716d84ab77623e068
Camille Deschain <fernando.maldonadogtz@gmail.com>
2022-03-29 21:30:01 -0600

    actualizacion angular

--- Frontend/angular-crud-basic/src/app/app.component.ts
+++ Frontend/angular-crud-basic/src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud-basic';
+  title: string = 'Angular Crud';
+  msg: string = '';
+  employees = [
+    { 'name': 'Fernando', 'position': 'Manager', 'email': 'a@gmail.com' },
+    { 'name': 'Camille', 'position': 'Musician', 'email': 'b@gmail.com' },
+    { 'name': 'Roland', 'position': 'Gunslinger', 'email': 'c@gmail.com' }
+  ];
+
+  model: any = {};
+  model2: any = {};
+  hideUpdate:boolean=true;
+  addEmployee(): void {
+    this.employees.push(this.model);
+    this.model = {}
+    this.msg = 'Campo Agregado'
+  }
+  deleteEmployee(i): void {
+    var answet = confirm("Estas Seguro?");
+    if (answet) {
+      this.employees.splice(i, 1);
+    }
+    this.msg='campo eliminado'
+  }
+  myValue;
+  editEmployee(i): void {
+    this.hideUpdate=false;
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    this.myValue = i;
+    this.msg='Editar Campo'
+  }
+  updateEmployee(): void {
+    let i = this.myValue;
+    for (let j=0; j < this.employees.length; j++) {
+      if (i == j) {
+        this.employees[i] = this.model2;
+        this.msg='campo actualizado'
+        this.model2 = {};
+      }
+    }
+  }
+  closeAlert() {
+    this.msg = ''
+  }
 }
