a5c02603b4cf00f70dc01ec2c4c7380f6af2ed12
Camille Deschain <fernando.maldonadogtz@gmail.com>
2022-04-28 11:26:33 -0500

    act

---
+++ BusquedasDinamicasAngularSpring/Backend/BusquedaDinamica/src/main/java/com/tw/BusquedaDinamica/BusquedaDinamicaApplication.java
@@ -0,0 +1,13 @@
+package com.tw.BusquedaDinamica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusquedaDinamicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusquedaDinamicaApplication.class, args);
+	}
+
+}
---
+++ BusquedasDinamicasAngularSpring/Backend/BusquedaDinamica/src/test/java/com/tw/BusquedaDinamica/BusquedaDinamicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.BusquedaDinamica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusquedaDinamicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
