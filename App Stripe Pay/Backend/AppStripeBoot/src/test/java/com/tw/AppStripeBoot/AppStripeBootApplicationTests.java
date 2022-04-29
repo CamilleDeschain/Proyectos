70cf51a5e5a03f56a85c899053975cb2ca92d705
Camille Deschain <fernando.maldonadogtz@gmail.com>
2022-04-29 17:44:00 -0500

    Actualizacion

---
+++ App Stripe Pay/Backend/AppStripeBoot/src/test/java/com/tw/AppStripeBoot/AppStripeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.AppStripeBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppStripeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
