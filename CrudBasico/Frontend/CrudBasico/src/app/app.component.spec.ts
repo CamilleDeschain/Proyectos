9d10a4d435840ee02affa9d6f3c7ffe3d7ad8dea
Camille Deschain <fernando.maldonadogtz@gmail.com>
2022-06-21 14:24:51 -0500

    Update app.component.spec.ts

--- CrudBasico/Frontend/CrudBasico/src/app/app.component.spec.ts
+++ CrudBasico/Frontend/CrudBasico/src/app/app.component.spec.ts
@@ -17,15 +17,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'paisespaginacionFRONT'`, async(() => {
+  it(`should have as title 'productospaginacionFRONT'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('paisespaginacionFRONT');
+    expect(app.title).toEqual('productospaginacionFRONT');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to paisespaginacionFRONT!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to productospaginacionFRONT!');
   }));
 });
